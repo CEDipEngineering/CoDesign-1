c018a30b578f1d5cde11f711de21be11ef2194be
Carlos Dip <cadudip@hotmail.com>
2019-09-16 17:34:08 -0300

    Completed layout and javaScript.

--- assets/js/projeto.js
+++ assets/js/projeto.js
@@ -1,24 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-let db = coDesConnect("https://codesign-2019-a.firebaseio.com/")
+  let db = coDesConnect("https://codesign-2019-a.firebaseio.com/")
+
+  let doc = document.querySelector('.main');
+  doc.classList.add('hidden');
+
+  let cover = document.querySelector('.cover');
+  cover.classList.remove('hidden');
 
   db.download('/', function(data) {
     const val = coDesExtract();
-  	var id = val['id'];
-  	map = MapDataBase(data);
-  	let cat = map[id];
+    var id = val['id'];
+    map = MapDataBase(data);
+    let cat = map[id];
 
     console.log(cat);
+    data['portfolio'][cat]["projetos"][id]['projectkey'] = id
+    let context = data['portfolio'][cat]["projetos"][id];
 
-  	let context = data['portfolio'][cat]["projetos"][id];
+    console.log(context);
 
-  	console.log(context);
-
-  	let body = {};
-  	body["main-body"] = {};
-  	for (line in data['portfolio'][cat]["projetos"][id]["main-body"]) {
-  		body["main-body"]["subtitles"] = data['portfolio'][cat]["projetos"][id]["main-body"]["subtitles"]
- 		body["main-body"]["text"] = data['portfolio'][cat]["projetos"][id]["main-body"]["text"]
-  	}
+    let body = {};
+    body["main-body"] = {};
+    for (line in data['portfolio'][cat]["projetos"][id]["main-body"]) {
+      body["main-body"]["subtitles"] = data['portfolio'][cat]["projetos"][id]["main-body"]["subtitles"]
+      body["main-body"]["text"] = data['portfolio'][cat]["projetos"][id]["main-body"]["text"]
+    }
 
     let titles = [];
     let contents = [];
@@ -32,7 +38,7 @@ let db = coDesConnect("https://codesign-2019-a.firebaseio.com/")
     }
     for (let i = 0; i<titles.length; i++) {
       constructorBody[i] = {'subtitle':titles[i],
-                            'body':contents[i]}
+      'body':contents[i]}
     }
 
     let members = [];
@@ -47,34 +53,40 @@ let db = coDesConnect("https://codesign-2019-a.firebaseio.com/")
     }
     for (let i = 0; i<members.length; i++) {
       constructorMenu[i] = {'member':members[i],
-                            'qualification':qualifications[i]}
+      'qualification':qualifications[i]}
     }
 
 
     let mainBodyBuilder = {'main-body': constructorBody};
     let menuBuilder = {'menuBuilder': constructorMenu};
 
-   //  console.log(titles);
-   // console.log(contents);
-   //  console.log(mainBodyBuilder);
-    console.log(menuBuilder);
+//  console.log(titles);
+// console.log(contents);
+//  console.log(mainBodyBuilder);
+console.log(menuBuilder);
 
-	  coDesReplace('.main-title', context);
-    coDesReplace('.sub-header', context);
-    coDesReplace('.main-body', mainBodyBuilder);
-  	coDesReplace('.menu', menuBuilder);
+coDesReplace('.main-title', context);
+coDesReplace('.sub-header', context);
+coDesReplace('.main-body', mainBodyBuilder);
+coDesReplace('.menu', menuBuilder);
 
-  })
+let doc = document.querySelector('.main');
+doc.classList.remove('hidden');
 
-function MapDataBase(object) {
-	map = {}
-	for(let directory in object["portfolio"]) {
-		for (let project in object["portfolio"][directory]["projetos"]) {
-			map[project] = directory;
-		}
-	}
-	return map
-}
+let cover = document.querySelector('.cover');
+cover.classList.add('hidden');
+
+})
+
+  function MapDataBase(object) {
+    map = {}
+    for(let directory in object["portfolio"]) {
+      for (let project in object["portfolio"][directory]["projetos"]) {
+        map[project] = directory;
+      }
+    }
+    return map
+  }
 
 
 
